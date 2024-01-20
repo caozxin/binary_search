c4ce7f084e71a647f78ee655b87881d8f223afd0
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2024-01-20 12:27:29 -0500

    Update find_missing.py

--- find_missing.py
+++ find_missing.py
@@ -1,1 +1,26 @@
 #Given an array contains N numbers of 0 .. N, find which number doesn't exist in the array.
+
+from typing import (
+    List,
+)
+
+class Solution:
+    """
+    @param nums: An array of integers
+    @return: An integer
+    """
+    def find_missing(self, nums: List[int]) -> int:
+        # write your code here
+        n = len(nums)
+        left, right = 0, n - 1
+        
+        while left <= right: 
+            mid = left + (right - left) //2
+            # print(left, right, mid)
+            print("mid, nums[mid]")
+            print(mid, nums[mid])
+            if nums[mid] < mid:  #move to left
+                right = mid - 1
+            else: # move to right
+                left = mid + 1
+            
