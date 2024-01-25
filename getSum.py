9488abc65094daeb08ade8547a5cc55cb08c9c40
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2024-01-25 04:00:14 -0500

    Update getSum.py

--- getSum.py
+++ getSum.py
@@ -1,2 +1,15 @@
 #371. Sum of Two Integers
 #Given two integers a and b, return the sum of the two integers without using the operators + and -.
+
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        default_str = '*'
+        print(a, b)
+
+        if not a or not b:
+            return 0
+
+        # a_str = default_str * a
+        print(len(default_str*b))
+
+        return 0
