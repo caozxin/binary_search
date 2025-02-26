60d97e49516e3a55d54692d68d5feb455e4d3a29
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2025-02-26 13:03:20 -0500

    Update nextGreatestLetter.py

--- nextGreatestLetter.py
+++ nextGreatestLetter.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List # remember to import list!
 
 class Solution:
     def nextGreatestLetter(self, letters: List[str], target: str) -> str:
@@ -6,13 +6,16 @@ class Solution:
         # return True if "a" < "c" else False  #python could directly compare letters based on their alphabetical order! 
 
         left, right = 0, len(letters) - 1
+        res_idx = -1
         
         while left <= right:
             mid = (left + right) // 2
             if letters[mid] > target:
                 right = mid - 1
+                res_idx = mid
             else:
                 left = mid + 1  # Move right to find a greater letter
         
         # If no greater letter is found, return the first letter (circular array behavior)
-        return letters[left] if left < len(letters) else letters[0]
+        # return letters[left] if left < len(letters) else letters[0]
+        return letters[res_idx] if res_idx >= 1 else letters[0]
