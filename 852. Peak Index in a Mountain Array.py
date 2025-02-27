acf6baa0e76a0214c5718d6391d09a5cf1c8637d
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2025-02-27 10:38:27 -0500

    Update 852. Peak Index in a Mountain Array.py

--- 852. Peak Index in a Mountain Array.py
+++ 852. Peak Index in a Mountain Array.py
@@ -21,3 +21,21 @@ class Solution:
         
 
         return max_idx if max_idx != -1 else 0
+
+#second verion of the same time & space complexity and it is more neat:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        
+        alen = len(arr)
+        left = 0
+        right = alen - 1
+        boundary_index = -1
+
+        while left <= right:
+            mid = (left + right) // 2
+            if arr[mid] > arr[mid + 1]:
+                boundary_index = mid
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return boundary_index
